7b1d4542680321defae430aa44cfd541855485c2
Gustavo Melo <gustavobarbosa-18@hotmail.com>
2020-06-18 13:21:06 -0300

    Melhorias em Loans

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(ReaderTableSeeder::class);
-        $this->call(LoanTableSeeder::class);
         $this->call(BooksTableSeeder::class);
     }
 }
